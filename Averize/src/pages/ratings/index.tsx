4c650db2c13f486d975b0e877e8b5ec34bff0ed4
Gustavo Braz <brazgustav@gmail.com>
2023-08-10 23:56:06 -0300

    index.tsx - Preventing the keyboard from overlapping
    Fixing bugs.

--- Averize/src/pages/ratings/index.tsx
+++ Averize/src/pages/ratings/index.tsx
@@ -3,6 +3,7 @@ import { Box, Heading, Input, Text, HStack, IconButton, ArrowForwardIcon } from 
 import { StackParamList } from '../../types/StackNavigation'
 import { Controller, useForm } from 'react-hook-form'
 import { useRatingsScreen } from '../../hooks/useRatingsScreen'
+import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native'; 
 
 type RouteProps = RouteProp<StackParamList, 'ratings'>
 
@@ -26,70 +27,79 @@ export default function Ratings() {
     }
 
     return (
-        <Box
-            backgroundColor={'white'}
-            flex={1}
-            justifyContent={'center'}
-            alignItems={'center'}
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'} // Adjust behavior according to platform
+            style={{ flex: 1 }}
         >
-            <Box width={'2/3'}>
-                <Heading fontSize={'2xl'} marginBottom={10} textAlign={'center'}>{subject}</Heading>
-                <Text marginBottom={5} fontSize={'lg'}>1° bimestre</Text>
-                <HStack space={3} marginBottom={5}>
-                    <Controller control={control} name='FBN1' render={({ field: { onChange } }) => (
-                        <Input
-                            keyboardType='number-pad'
-                            placeholder='N1'
-                            w={'1/4'}
-                            onChangeText={onChange} />
-                    )} />
-                    <Controller control={control} name='FBN2' render={({ field: { onChange } }) => (
-                        <Input
-                            keyboardType='number-pad'
-                            placeholder='N2'
-                            w={'1/4'}
-                            onChangeText={onChange} />
-                    )} />
-                </HStack>
-                <Text marginBottom={5} fontSize={'lg'}>2° bimestre</Text>
-                <HStack space={3} marginBottom={5}>
-                    <Controller control={control} name='SBN1' render={({ field: { onChange } }) => (
-                        <Input
-                            keyboardType='number-pad'
-                            placeholder='N1'
-                            w={'1/4'}
-                            onChangeText={onChange} />
-                    )} />
-                    <Controller control={control} name='SBN2' render={({ field: { onChange } }) => (
-                        <Input
-                            keyboardType='number-pad'
-                            placeholder='N2'
-                            w={'1/4'}
-                            onChangeText={onChange} />
-                    )} />
-                    <Controller control={control} name='AF' render={({ field: { onChange } }) => (
-                        <Input
-                            keyboardType='number-pad'
-                            placeholder='AF'
-                            w={'1/4'}
-                            onChangeText={onChange} />
-                    )} />
-                </HStack>
-                <Box alignItems={'flex-end'}>
-                    <IconButton
-                        icon={<ArrowForwardIcon />}
-                        onPress={handleSubmit(handleRatings)}
-                        _icon={{
-                            color: 'black',
-                            size: 'md',
-                        }} _hover={{
-                            bg: 'white',
-                        }} _pressed={{
-                            bg: 'white',
-                        }}
-                    />
+            <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+            <Box
+                backgroundColor={'white'}
+                flex={1}
+                justifyContent={'center'}
+                alignItems={'center'}
+            >
+                <Box width={'2/3'}>
+                <KeyboardAvoidingView behavior="padding">
+                    <Heading fontSize={'2xl'} marginBottom={10} textAlign={'center'}>{subject}</Heading>
+                    <Text marginBottom={5} fontSize={'lg'}>1° bimestre</Text>
+                    <HStack space={3} marginBottom={5}>
+                        <Controller control={control} name='FBN1' render={({ field: { onChange } }) => (
+                            <Input
+                                keyboardType='number-pad'
+                                placeholder='N1'
+                                w={'1/4'}
+                                onChangeText={onChange} />
+                        )} />
+                        <Controller control={control} name='FBN2' render={({ field: { onChange } }) => (
+                            <Input
+                                keyboardType='number-pad'
+                                placeholder='N2'
+                                w={'1/4'}
+                                onChangeText={onChange} />
+                        )} />
+                    </HStack>
+                    <Text marginBottom={5} fontSize={'lg'}>2° bimestre</Text>
+                    <HStack space={3} marginBottom={5}>
+                        <Controller control={control} name='SBN1' render={({ field: { onChange } }) => (
+                            <Input
+                                keyboardType='number-pad'
+                                placeholder='N1'
+                                w={'1/4'}
+                                onChangeText={onChange} />
+                        )} />
+                        <Controller control={control} name='SBN2' render={({ field: { onChange } }) => (
+                            <Input
+                                keyboardType='number-pad'
+                                placeholder='N2'
+                                w={'1/4'}
+                                onChangeText={onChange} />
+                        )} />
+                        <Controller control={control} name='AF' render={({ field: { onChange } }) => (
+                            <Input
+                                keyboardType='number-pad'
+                                placeholder='AF'
+                                w={'1/4'}
+                                onChangeText={onChange} />
+                        )} />
+                    </HStack>
+                    <Box alignItems={'flex-end'}>
+                        <IconButton
+                            icon={<ArrowForwardIcon />}
+                            onPress={handleSubmit(handleRatings)}
+                            _icon={{
+                                color: 'black',
+                                size: 'md',
+                            }} _hover={{
+                                bg: 'white',
+                            }} _pressed={{
+                                bg: 'white',
+                            }}
+                        />
+                    </Box>
+                    </KeyboardAvoidingView>
                 </Box>
             </Box>
-        </Box>
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
